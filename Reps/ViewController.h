9d8a408fe6162bbfe2c0885eaac089f55afa6863
Gabriel Carrillo <ecarrillo@alumni.utexas.net>
2015-12-19 09:40:45 -0800

    Initial checkin

---
+++ Reps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reps
+//
+//  Created by Gabriel Carrillo on 12/19/15.
+//  Copyright © 2015 Gabriel Carrillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RepsTests/RepsTests.m
@@ -0,0 +1,39 @@
+//
+//  RepsTests.m
+//  RepsTests
+//
+//  Created by Gabriel Carrillo on 12/19/15.
+//  Copyright © 2015 Gabriel Carrillo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RepsTests : XCTestCase
+
+@end
+
+@implementation RepsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
